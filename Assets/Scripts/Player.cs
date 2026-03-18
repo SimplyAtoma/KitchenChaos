c5b7798c2a12f93d7616d302f376682ae6fc46d6
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2026-03-18 12:50:43 -0700

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public InputManager inputManager;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
