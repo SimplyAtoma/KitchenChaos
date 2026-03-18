0431aad864381e2c56eee0b35681f69085039046
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2026-03-18 16:06:23 -0700

    input

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,32 +3,15 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float speed = 7.0f;
+    [SerializeField] private GameInput gameInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     // Update is called once per frame
     private bool isWalking = false;
     void Update()
     { 
-        Vector2 inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y += 1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y -= 1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x -= 1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x += 1;
-        }
-        inputVector = inputVector.normalized;
-
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 movement = new Vector3(inputVector.x, 0, inputVector.y);
         transform.position += movement * speed * Time.deltaTime;
         isWalking = movement != Vector3.zero;
@@ -36,10 +19,6 @@ public class Player : MonoBehaviour
         transform.forward = Vector3.Slerp(transform.forward, movement, Time.deltaTime*rotateSpeed);
     }   
 
-    public void Interact()
-    {
-        Debug.Log("Interacting");
-    }
 
     public bool IsWalking()
     {
