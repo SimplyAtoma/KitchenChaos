be00a4857ac441079234ed3aa4e91f5d0938545d
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2026-03-18 15:15:32 -0700

    animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,17 +3,46 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public InputManager inputManager;
+    [SerializeField] private float speed = 5.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
+    private bool isWalking = false;
     void Update()
+    { 
+        Vector2 inputVector = new Vector2(0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y += 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y -= 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x -= 1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x += 1;
+        }
+        inputVector = inputVector.normalized;
+
+        Vector3 movement = new Vector3(inputVector.x, 0, inputVector.y);
+        transform.position += movement * speed * Time.deltaTime;
+        isWalking = movement != Vector3.zero;
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, movement, Time.deltaTime*rotateSpeed);
+    }   
+
+    public void Interact()
     {
-        
+        Debug.Log("Interacting");
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private const string IS_WALKING = "IsWalking";
+    private Animator animator;
+    [SerializeField] private Player player;
+
+    // Update is called once per frame
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetBool(IS_WALKING, player.IsWalking());
+    }
+    private void Update()
+    {
+        animator.SetBool(IS_WALKING, player.IsWalking());
+    }
+}
